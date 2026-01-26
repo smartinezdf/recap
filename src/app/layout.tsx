dd5dedf0eab0a5ad5252ec9d048e87d0a3b7dae8
smartinezdf <smartinezdf@gmail.com>
2026-01-26 01:17:08 -0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,14 +16,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="es">
-      <head>
-        {/* Tab favicon */}
-        <link rel="icon" href="/Recap.png" />
-
-        {/* iPhone/iPad: Add to Home Screen */}
-        <link rel="apple-touch-icon" href="/Recap.png" />
-      </head>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+      </body>
     </html>
   );
 }
